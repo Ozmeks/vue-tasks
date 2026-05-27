a862faedd874c6ec9a7559911d794b56ac5a7f71
ozmeks <omehryakov@gmail.com>
2026-05-27 20:27:51 +0200

    Modify card template

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,9 @@
   <div class="divider"></div>
   <div class="main-content">
     <Button v-if="!isStarted" @click="startGame">Начать игру</Button>
-    <Card v-if="isStarted" :data="cardData" />
+    <div v-if ="isStarted" class="cards-container">
+      <Card v-for="(cardData, index) in cards" :key="cardData.key" :data="cardData" />
+    </div>
   </div>
 </template>
 
@@ -21,12 +23,34 @@ import { ref } from 'vue';
 let score = ref("100");
 let isStarted = ref(false);
 
-const cardData = ref({
-  word: 'test word',
-  translation: 'тестовое слово',
+const cards = ref([{
+  key: 1,
+  word: 'Aufschlag',
+  translation: 'Подача',
   state: 'closed', // possible values: 'closed', 'open'
   status: 'pending' // possible values: 'pending', 'success', 'fail'
-});
+},
+{
+  key: 2,
+  word: 'Bagger',
+  translation: 'приём снизу',
+  state: 'open',
+  status: 'pending'
+},
+{
+  key: 3,
+  word: 'flach',
+  translation: 'низкий',
+  state: 'open',
+  status: 'success'
+},
+{
+  key: 4,
+  word: 'vorne',
+  translation: 'впереди',
+  state: 'open',
+  status: 'fail'
+}]);
 
 function startGame() {
   isStarted.value = true; 
@@ -54,5 +78,12 @@ function startGame() {
   display: flex;
   justify-content: center;
   padding: 20px;
+}
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+  justify-content: center;
+  width: 100%;
 }
 </style>
